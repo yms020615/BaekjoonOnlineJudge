9021c66ec3e3966dcc62a81cd602d889fd245cff
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-05-16 21:30:22 +0900

    Create 1927.py

---
+++ 1927.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+from heapq import *
+
+heap = []
+n = int(input())
+for _ in range(n):
+    x = int(input())
+    if x == 0:
+        if not heap:
+            print(0)
+        else:
+            print(heappop(heap))
+    else:
+        heappush(heap, x)
