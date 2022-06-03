1bcfdb22ed93896437a8c20f82ab5a1d236448a7
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-06-04 00:15:26 +0900

    Create 2588.c

---
+++ 2588.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d\n", a * (b % 10));
+    printf("%d\n", a * ((b / 10) % 10));
+    printf("%d\n", a * ((b / 100) % 10));
+    printf("%d", a * b);
+}
