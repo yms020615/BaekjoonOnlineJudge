1fc1b0b714277b16e09000f4b94f5ada948ed5c6
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-06-14 01:03:48 +0900

    Create 5095.py

---
+++ 5095.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+def multiply(mat1, mat2, n):
+    ans = [[0] * n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            ans[i][j] = sum(mat1[i][k] * mat2[k][j] for k in range(n)) % c
+
+    return ans
+
+def power(mat, p):
+    if p == 1:
+        return mat
+    else:
+        temp = power(mat, p // 2)
+        if p % 2 == 0:
+            return multiply(temp, temp, n)
+        else:
+            return multiply(multiply(temp, temp, n), mat, n)
+
+while True:
+    n, c, p = map(int, input().split())
+    if n == c == p == 0:
+        break
+    m = []
+    for i in range(n):
+        m.append(list(map(int, input().split())))
+    p = power(m, p)
+    for i in p:
+        for j in i:
+            print(j % c, end=' ')
+        print()
+    print()
