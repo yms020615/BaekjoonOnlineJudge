320c9abec6835f7f0c365b4016ae7ffe7f0d178b
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-07-01 15:40:52 +0900

    Create 11052.py

---
+++ 11052.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+from itertools import combinations
+
+n = int(input())
+a = list(map(int, input().split()))
+a.insert(0, 0)
+dp = [0] * (n+1)
+dp[1] = a[1]
+dp[2] = max(a[2], 2*a[1])
+for i in range(3, n+1):
+    dp[i] = a[i]
+    for j in range(1, i//2+1):
+        dp[i] = max(dp[i], dp[j] + dp[i-j])
+print(dp[n])
