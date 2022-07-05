23c7c880058199b6b55cb5369b698f17d53a5445
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-07-05 18:52:32 +0900

    Create 11442.py

---
+++ 11442.py
@@ -0,0 +1,29 @@
+import sys
+input = sys.stdin.readline
+
+c = 1000000007
+
+def multiply(mat1, mat2):
+    ans = [[0, 0], [0, 0]]
+    ans[0][0] = (mat1[0][0] * mat2[0][0] + mat1[0][1] * mat2[1][0]) % c
+    ans[0][1] = (mat1[0][0] * mat2[0][1] + mat1[0][1] * mat2[1][1]) % c
+    ans[1][0] = (mat1[1][0] * mat2[0][0] + mat1[1][1] * mat2[1][0]) % c
+    ans[1][1] = (mat1[1][0] * mat2[0][1] + mat1[1][1] * mat2[1][1]) % c
+    return ans
+
+def power(mat, p):
+    if p == 1:
+        return mat
+    else:
+        temp = power(mat, p // 2)
+        if p % 2 == 0:
+            return multiply(temp, temp)
+        else:
+            return multiply(multiply(temp, temp), mat)
+
+m = [[1, 1], [1, 0]]
+a = int(input())
+if a % 2 == 0:
+    print(power(m, a)[0][1])
+else:
+    print(power(m, a)[0][0])
