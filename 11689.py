0368696c658498ee31c772b039a1f31a09be3fcb
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-07-10 00:13:34 +0900

    Create 11689.py

---
+++ 11689.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+ans = n = int(input())
+
+for i in range(2, int(n ** 0.5) + 1):
+    if n % i == 0:
+        while n % i == 0:
+            n //= i
+        ans *= 1 - 1 / i
+
+if n > 1:
+    ans *= 1 - (1 / n)
+
+print(round(ans))
