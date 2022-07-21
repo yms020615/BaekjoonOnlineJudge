19ef53535fc69a8318086358320b9f06723614e1
Yoon Minseo <97289852+bidulki-99@users.noreply.github.com>
2022-07-21 18:02:13 +0900

    Create 14428.py

---
+++ 14428.py
@@ -0,0 +1,48 @@
+import sys
+input = sys.stdin.readline
+
+def initMin(start, end, node):
+    if start == end:
+        tree[node] = a[start]
+        return tree[node]
+
+    mid = (start + end) // 2
+    tree[node] = min(initMin(start, mid, node * 2), initMin(mid + 1, end, node * 2 + 1))
+    return tree[node]
+
+def queryMin(start, end, node, left, right):
+    if start > right or end < left:
+        return float('inf')
+
+    if left <= start and end <= right:
+        return tree[node]
+
+    mid = (start + end) // 2
+    return min(queryMin(start, mid, node * 2, left, right), queryMin(mid + 1, end, node * 2 + 1, left, right))
+
+def segment_update(start, end, node, index, diff):
+    if index < start or index > end:
+        return
+    
+    if start == end:
+        tree[node] = diff
+        return
+
+    mid = (start + end) // 2
+    segment_update(start, mid, node * 2, index, diff)
+    segment_update(mid + 1, end, node * 2 + 1, index, diff)
+    tree[node] = min(tree[node * 2], tree[node * 2 + 1])
+
+n = int(input())
+a = list(map(int, input().split()))
+tree = [0 for _ in range(4*n)]
+initMin(0, n-1, 1)
+
+m = int(input())
+for _ in range(m):
+    q1, q2, q3 = map(int, input().split())
+    if q1 == 1:
+        a[q2-1] = q3
+        segment_update(0, n-1, 1, q2-1, q3)
+    else:
+        print(a.index(queryMin(0, n-1, 1, q2-1, q3-1), q2-1, q3) + 1)
